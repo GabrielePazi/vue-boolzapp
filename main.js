99c2bf6b9f6cf3430eea7c2e7e88bcf5a2083748
GabrielePazi <pazigabriele@gmail.com>
2023-08-03 18:20:13 +0200

    1.5.7.1 correzione indentazione

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 "use strict"
 
-
-
-
 const app = Vue.createApp({
   data() {
     return {
@@ -103,10 +100,22 @@ const app = Vue.createApp({
     openChat(contact) {
       this.currentContact = contact
 
+      this.scrollBottom()
+
       this.chatStatus = "Ultimo accesso oggi alle " + this.formatDate(this.currentContact.messages[this.currentContact.messages.length - 1].date)
     },
+    /**
+     * Crea una data.
+     * Controlla che il messaggio inserito non sia vuoto o composto da soli spazi.
+     * Crea un nuovo oggetto Messaggio con la data attuale creata prima, il testo insserito e formattato, il tipo di messaggio e poi lo aggiunge al contatto corrente.
+     * Resetta il valore del nuovo messaggio.
+     * Modifica lo stato della chat.
+     * Scrolla verso il basso per visualizzare il messaggio appena inviato.
+     * Dopo 1 sec. invia una tra le risposte radnomiche preimpostate e fa un altro scroll per visualizzare il messaggio appena ricevuto.
+     * Dopo 2 sec. modifica lo stato della chat
+     * 
+     */
     sendMessage() {
-
       const currentTime = new Date()
 
       if (this.newMessage !== "" && this.checkOnlySpaces(this.newMessage)) {
@@ -145,12 +154,22 @@ const app = Vue.createApp({
 
       return this.answers[rand]
     },
+    /**
+     * Converte una data in formato hh:mm
+     * @param {*} date data in formato "dd/mm/yyyy hh:mm:ss"
+     * @returns data convertita
+     */
     formatDate(date) {
       date = date.split(' ')[1]
 
       date = date.split(':')[0] + ":" + date.split(':')[1]
       return date
     },
+    /**
+     * Converte una stringa in una stringa con la prima lettera Maiuscola
+     * @param {*} string string da convertire
+     * @returns string con maiuscola
+     */
     capitalize(string) {
       let capitalizedString = ""
 
@@ -164,22 +183,23 @@ const app = Vue.createApp({
 
       return capitalizedString
     },
-    deleteMessage(testoMess, indexToDel, currentContact) {
-      /*funziona ma cancella tutti i messaggio con il testo inserito, non solo uno   
-      currentContact.messages = currentContact.messages.filter((el) => {
-        return el.message !== testoMess
-      })*/
-
-      for (let i = 0; i < currentContact.messages.length; i++) {
-        if (currentContact.messages[i].message == testoMess && indexToDel == i) {
-          currentContact.messages.splice(i, 1)
+    /**
+     * Cancella il messaggio il cui testo e indice vengono passati come primi due parametri 
+     * 
+     * @param {*} testoMess testo del messaggio da cancellare
+     * @param {*} indexToDel indice del messaggio da cancellare
+     */
+    deleteMessage(testoMess, indexToDel) {
+      for (let i = 0; i < this.currentContact.messages.length; i++) {
+        if (this.currentContact.messages[i].message == testoMess && indexToDel == i) {
+          this.currentContact.messages.splice(i, 1)
           break
         }
       }
     },
     checkOnlySpaces(str) {
       return !str.trim() == ""  //str.trim() restituisce la stringa senza spazi 
-    }, 
+    },
     scrollBottom() {
       this.$nextTick(() => {
         this.$refs.chatContainer.scrollTop = this.$refs.chatContainer.scrollHeight;
@@ -191,6 +211,5 @@ const app = Vue.createApp({
   },
   mounted() {
     this.chatStatus = "Ultimo accesso oggi alle " + this.formatDate(this.currentContact.messages[this.currentContact.messages.length - 1].date)
-    
   }
 }).mount('#app')
